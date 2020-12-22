b47f1d6c7430e6f705e529d9cdad9d6600c6f791
Junior RivasTorres <jrtimmobilier@gmail.com>
2020-12-21 20:32:59 -0500

    start the caroussel arrow

---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const menuData = [
+    { title: 'About', Link: '/about' },
+    { title: 'Homes', Link: '/homes' },
+    { title: 'Rentals', Link: '/rentals' },
+    { title: 'Vendu', Link: '/vendus' },
+    { title: 'Contacts', Link: '/contacts' }
+];
